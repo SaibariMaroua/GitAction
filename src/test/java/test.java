507212f530a83e48a077027989e90e0b9ad733ab
maroua saibari <saibari@gmail.com>
2024-05-22 01:17:03 +0100

    modification de test2

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,5 +1,7 @@
 import org.example.Main;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 
 import static org.junit.Assert.assertTrue;
 
