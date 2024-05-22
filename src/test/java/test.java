43535c5c5594c21fd2460e7cffcafac9ecae0bf9
maroua saibari <saibari@gmail.com>
2024-05-22 01:23:00 +0100

    modification de test5

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,19 +2,11 @@ import org.example.Main;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-
-import static org.junit.Assert.assertTrue;
-
 public class test {
 
- @Test
+    @Test
     public void testAdd() {
         Main main = new Main();
-        assertEquals(5, main.add(2, 7), "2 + 3 devrait être égal à 5");
+        assertEquals(5, main.add(2, 3), "2 + 3 devrait être égal à 5");
     }
-
-
-    }
-
-
-
+}
