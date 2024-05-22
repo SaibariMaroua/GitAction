88a1f15a6a00fb298153896b4471d6ed5ec3dc22
maroua saibari <saibari@gmail.com>
2024-05-22 01:32:26 +0100

    modification de test6

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,12 +1,13 @@
-import org.example.Main;
+package org.example;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-public class test {
+public class MainTest {
 
     @Test
     public void testAdd() {
         Main main = new Main();
-        assertEquals(5, main.add(2, 7), "2 + 3 devrait être égal à 5");
+        assertEquals(5, main.add(2, 9), "2 + 3 devrait être égal à 5");
     }
 }
