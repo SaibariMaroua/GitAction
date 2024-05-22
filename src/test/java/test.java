01fccfeb52409dd55d79457cde943b59f7923453
maroua saibari <saibari@gmail.com>
2024-05-22 01:00:31 +0100

    modification de fichier test.java

---
+++ src/test/java/test.java
@@ -0,0 +1,18 @@
+import org.example.Main;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class test {
+
+ @Test
+    public void testAdd() {
+        Main main = new Main();
+        assertEquals(5, main.add(2, 3), "2 + 3 devrait être égal à 5");
+    }
+
+
+    }
+
+
+
